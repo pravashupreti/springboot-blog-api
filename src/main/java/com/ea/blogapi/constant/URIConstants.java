4078a486aa16739553ac801df7eb6f2a8409c66d
pravash <pravash@pravash-mac.local>
2022-06-13 08:09:46 -0500

    Add constant

---
+++ src/main/java/com/ea/blogapi/constant/URIConstants.java
@@ -0,0 +1,15 @@
+package com.ea.blogapi.constant;
+
+public class URIConstants {
+
+    public static final String CONTENT_SERVICE_URL = "localhost";
+    public static final Integer CONTENT_SERVICE_PORT = 8081;
+
+    public static final String MEDIA_SERVICE_URL = "localhost";
+    public static final Integer MEDIA_SERVICE_PORT = 8082;
+
+    public static final String USER_SERVICE_URL = "localhost";
+    public static final Integer USER_SERVICE_PORT = 8083;
+
+
+}
