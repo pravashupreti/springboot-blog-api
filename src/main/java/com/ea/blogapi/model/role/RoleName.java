455f999c733dd3a067ad841933c132653544f542
pravash <pravash@pravash-mac.local>
2022-06-13 08:10:46 -0500

    Add model

---
+++ src/main/java/com/ea/blogapi/model/role/RoleName.java
@@ -0,0 +1,6 @@
+package com.ea.blogapi.model.role;
+
+public enum RoleName {
+	ROLE_ADMIN,
+	ROLE_USER,
+}
---
+++ src/main/java/com/ea/blogapi/model/user/User.java
@@ -0,0 +1,87 @@
+package com.ea.blogapi.model.user;
+
+
+import com.ea.blogapi.model.role.Role;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.NaturalId;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "users", uniqueConstraints = {@UniqueConstraint(columnNames = {"username"}),
+        @UniqueConstraint(columnNames = {"email"})})
+public class User {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NotBlank
+    @Column(name = "first_name")
+    @Size(max = 40)
+    private String firstName;
+
+    @NotBlank
+    @Column(name = "last_name")
+    @Size(max = 40)
+    private String lastName;
+
+    @NotBlank
+    @Column(name = "username")
+    @Size(max = 15)
+    private String username;
+
+    @NotBlank
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Size(max = 100)
+    @Column(name = "password")
+    private String password;
+
+    @NotBlank
+    @NaturalId
+    @Size(max = 40)
+    @Column(name = "email")
+    @Email
+    private String email;
+
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "website")
+    private String website;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private List<Role> roles;
+
+
+    public User(String firstName, String lastName, String username, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+
+    public List<Role> getRoles() {
+
+        return roles == null ? null : new ArrayList<>(roles);
+    }
+}
