f2eb57f2873afa366a896e9c54f9e8a10c0fb400
pravash <pravash@pravash-mac.local>
2022-06-13 08:09:07 -0500

    Add config

---
+++ src/test/java/com/ea/blogapi/SpringbootBlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ea.blogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBlogApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
