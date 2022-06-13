34e6488770eb35dfaf28355a8890bc2affbd8062
pravash <pravash@pravash-mac.local>
2022-06-13 08:12:10 -0500

    Add proxy classes

---
+++ src/main/java/com/ea/blogapi/proxy/CommentProxy.java
@@ -0,0 +1,52 @@
+package com.ea.blogapi.proxy;
+
+import com.ea.blogapi.payload.ProxyRequest;
+import com.ea.blogapi.security.CurrentUser;
+import com.ea.blogapi.security.UserPrincipal;
+import com.ea.blogapi.service.impl.CommonProxyImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.URISyntaxException;
+
+import static com.ea.blogapi.constant.URIConstants.CONTENT_SERVICE_PORT;
+import static com.ea.blogapi.constant.URIConstants.CONTENT_SERVICE_URL;
+
+@RestController
+@RequestMapping("/api/blogs/{blogId}/comments")
+public class CommentProxy {
+
+
+    @Autowired
+    CommonProxyImpl commonProxy;
+
+
+    @PostMapping
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity onlyUserAPIs(@RequestBody(required = false) String body, HttpMethod method, HttpServletRequest request) throws URISyntaxException {
+
+
+        return  commonProxy.forward(CONTENT_SERVICE_URL,CONTENT_SERVICE_PORT,new ProxyRequest(body, method,request));
+
+    }
+
+    @PutMapping("/{id}")
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    public ResponseEntity anyUserAPIs(@RequestBody(required = false) String body, HttpMethod method, HttpServletRequest request) throws URISyntaxException {
+       return  commonProxy.forward(CONTENT_SERVICE_URL,CONTENT_SERVICE_PORT,new ProxyRequest(body, method,request));
+
+    }
+
+
+    @RequestMapping("")
+    public ResponseEntity publicAPIs(@RequestBody(required = false) String body, HttpMethod method, HttpServletRequest request) throws URISyntaxException {
+        return  commonProxy.forward(CONTENT_SERVICE_URL,CONTENT_SERVICE_PORT,new ProxyRequest(body, method,request));
+
+    }
+
+}
---
+++ src/main/java/com/ea/blogapi/proxy/PhotoProxy.java
@@ -0,0 +1,48 @@
+package com.ea.blogapi.proxy;
+
+import com.ea.blogapi.payload.ProxyRequest;
+import com.ea.blogapi.service.impl.CommonProxyImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.URISyntaxException;
+
+import static com.ea.blogapi.constant.URIConstants.MEDIA_SERVICE_PORT;
+import static com.ea.blogapi.constant.URIConstants.MEDIA_SERVICE_URL;
+
+@RestController
+@RequestMapping("/api/photos")
+public class PhotoProxy {
+
+
+    @Autowired
+    CommonProxyImpl commonProxy;
+
+
+    @PostMapping
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity onlyUserAPIs(@RequestBody(required = false) String body, HttpMethod method, HttpServletRequest request) throws URISyntaxException {
+        return  commonProxy.forward(MEDIA_SERVICE_URL, MEDIA_SERVICE_PORT,new ProxyRequest(body, method,request));
+
+    }
+
+    @PutMapping("/{id}")
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    public ResponseEntity anyUserAPIs(@RequestBody(required = false) String body, HttpMethod method, HttpServletRequest request) throws URISyntaxException {
+       return  commonProxy.forward(MEDIA_SERVICE_URL, MEDIA_SERVICE_PORT,new ProxyRequest(body, method,request));
+
+    }
+
+
+    @RequestMapping("")
+    public ResponseEntity publicAPIs(@RequestBody(required = false) String body, HttpMethod method, HttpServletRequest request) throws URISyntaxException {
+        return  commonProxy.forward(MEDIA_SERVICE_URL, MEDIA_SERVICE_PORT,new ProxyRequest(body, method,request));
+
+    }
+
+}
